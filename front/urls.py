e54fe9b91cb486129c3b8d45d08fa798a5ad93d3
jjuya <jjuya9381@naver.com>
2018-01-23 03:10:54 +0900

    make board_new, edit static

--- front/urls.py
+++ front/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.board_list, name='board_list'),
     url(r'^board/(?P<pk>\d+)/$', views.board_detail, name='board_detail'),
+    url(r'^board/new/$', views.board_new, name='board_new'),
 ]
--- front/views.py
+++ front/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 def board_list(request):
@@ -11,3 +12,16 @@ def board_detail(request, pk):
     board = get_object_or_404(Board, pk = pk)
 
     return render(request, 'board/board_detail.html', {'board' : board})
+
+def board_new(request):
+    if request.method == "POST":
+        form = BoardForm(request.POST)
+
+        if form.is_valid():
+            board = form.save()
+
+            return redirect('board_detail', pk=board.pk)
+    else:
+        form = BoardForm()
+
+    return render(request, 'board/board_new.html', {'form' : form})
