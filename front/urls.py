de5b10453062dd5780d02a9a6d66a99c0e90d661
jjuya <jjuya9381@naver.com>
2018-01-23 02:12:27 +0900

    make board_detail

--- front/urls.py
+++ front/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.board_list, name='board_list'),
+    url(r'^board/(?P<pk>\d+)/$', views.board_detail, name='board_detail'),
 ]
--- front/views.py
+++ front/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
@@ -6,3 +6,8 @@ from .models import *
 def board_list(request):
     boards = Board.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'board/board_list.html', {'boards' : boards})
+
+def board_detail(request, pk):
+    board = get_object_or_404(Board, pk = pk)
+
+    return render(request, 'board/board_detail.html', {'board' : board})
