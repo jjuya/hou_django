97a898bc5476da2efccd4f4a51ea3c2c6e446012
jjuya <jjuya9381@naver.com>
2018-01-24 07:44:23 +0900

    make list_model

--- front/models.py
+++ front/models.py
@@ -15,3 +15,18 @@ class Board(models.Model):
 
     def __str__(self):
         return self.title
+
+class List(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
